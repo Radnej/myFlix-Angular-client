6bd3c7b3606bef67787b6cf466e73d87e0827c96
Radnej <91905344+Radnej@users.noreply.github.com>
2022-09-25 11:00:16 +0200

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -60,6 +60,18 @@ private handleError(error: HttpErrorResponse): any {
       .pipe(map(this.extractResponseData), catchError(this.handleError));
   }
 
+  //API call to get one movie endpoint
+  public getOneMovie(title: string): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http
+      .get(`${apiUrl}movies/${title}`, {
+        headers: new HttpHeaders({
+          Authorization: `Bearer ${token}`,
+        }),
+      })
+      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  }
+
     // Non-typed response extraction
     private extractResponseData(res: any): any {
       const body = res;
