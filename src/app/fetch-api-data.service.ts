69cae8324eb031538ae02610a5aa3ffb51995fcf
Radnej <91905344+Radnej@users.noreply.github.com>
2022-09-25 11:35:20 +0200

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -135,6 +135,19 @@ private handleError(error: HttpErrorResponse): any {
       .pipe(map(this.extractResponseData), catchError(this.handleError));
   }
 
+  // API call to get data of a user and update user data endpoint
+  public  updateUser(): Observable<any> {
+    const token = localStorage.getItem('token');
+    const user = localStorage.getItem('user');
+    return this.http
+      .put(`${apiUrl}users/${user}`, {
+        headers: new HttpHeaders({
+          Authorization: `Bearer ${token}`,
+        }),
+      })
+      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  }
+  
     // Non-typed response extraction
     private extractResponseData(res: any): any {
       const body = res;
