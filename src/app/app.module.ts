7831eb2d600438d466caa3b9ca59179433cc597c
Radnej <91905344+Radnej@users.noreply.github.com>
2022-09-27 19:19:36 +0200

    Update app.module.ts
    install Angular Material and import Material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,12 @@
 import { NgModule } from '@angular/core';
+//Material components
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
 //simplified API for Angular applications that makes it possible for the client app to communicate with the API or server-side
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
@@ -13,7 +21,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
