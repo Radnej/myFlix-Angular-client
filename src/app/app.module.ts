a844474c88c739ffe374cb02fbb9708d7ae49e8e
Radnej <91905344+Radnej@users.noreply.github.com>
2022-09-27 22:41:52 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
-//Material components
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { FormsModule } from '@angular/forms';
-
+import { MatAutocompleteModule, MatButtonModule, MatCheckboxModule, MatDatepickerModule, 
+  MatFormFieldModule, MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule, 
+  MatSlideToggleModule } from '@angular/material';
+  
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -21,16 +17,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    MatInputModule,
+    MatAutocompleteModule,
     MatButtonModule,
-    MatCardModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
     MatFormFieldModule,
-    MatDialogModule,
-    MatSnackBarModule,
-    FormsModule
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule
 
   ],
   providers: [],
