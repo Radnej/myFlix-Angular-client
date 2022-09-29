3232f602e5b420fd93631ff56a653a351fc6a217
Radnej <91905344+Radnej@users.noreply.github.com>
2022-09-29 20:12:17 +0200

    install material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,27 @@
 import { NgModule } from '@angular/core';
 
+
+
 //simplified API for Angular applications that makes it possible for the client app to communicate with the API or server-side
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MatAutocompleteModule, MatButtonModule, MatCheckboxModule, MatDatepickerModule, 
-  MatFormFieldModule, MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule, 
-  MatSlideToggleModule } from '@angular/material';
-  
- 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    MatAutocompleteModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatRadioModule,
-    MatSelectModule,
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
     MatSliderModule,
-    MatSlideToggleModule
-
+    
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
