500869a7b97a5a8fb61cba20baea36f2f0c287f2
Radnej <91905344+Radnej@users.noreply.github.com>
2022-09-30 18:08:25 +0200

    install angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,4 @@
 import { NgModule } from '@angular/core';
-
-
-
 //simplified API for Angular applications that makes it possible for the client app to communicate with the API or server-side
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
@@ -10,6 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +25,14 @@ import { MatSliderModule } from '@angular/material/slider';
     HttpClientModule,
     AppRoutingModule,
     MatSliderModule,
-    
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    FormsModule
    
   ],
   providers: [],
